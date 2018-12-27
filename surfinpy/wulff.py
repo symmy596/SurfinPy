46dd39d724d3d96a2b2cf0baf510c50cc057fc08
symmy596 <ars44@bath.ac.uk>
2018-12-27 18:19:42 -0100

    Updated examples

--- surfinpy/wulff.py
+++ surfinpy/wulff.py
@@ -14,20 +14,17 @@ def calculate_surface_energy(stoich,
                              coverage=None):
     if coverage is None:
         coverage = ut.calculate_coverage(data)
+
     R = codata.value('molar gas constant')
     N_A = codata.value('Avogadro constant')
-    P = np.log(10 ** P)
-    Tarr = np.arange(2, 1000)
-    shift = ut.fit(thermochem, Tarr)
-    shift = (Tarr * (shift / 1000)) / 96.485
-    for i in range(0, shift.size):
-        if Tarr[i] == T:
-            X = shift[i]
-    adsorbant = adsorbant - X
-    AE = pt.calculate_adsorption_energy(data, stoich, adsorbant)
-    Y = P * (T * R)
+    lnP = np.log(10 ** P)
+    temperature = np.arange(2, 1000)
+    shift = ut.fit(thermochem[:,0], thermochem[:,2], temperature)
+    shift = (T * (shift[(T - 1)] / 1000)) / 96.485   
+    adsorbant = adsorbant - shift
+    AE = pt.adsorption_energy(data, stoich, adsorbant)
     SEs = np.array([SE])
     for i in range(0, len(data)):
-        S = SE + (coverage[i] / N_A) * (AE[i] - Y)
+        S = SE + (coverage[i] / N_A) * (AE[i] - (lnP * (T * R)))
         SEs = np.append(SEs, S)
     return SEs
