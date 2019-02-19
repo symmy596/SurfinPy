ddaf2110728159dbb67e8e92a7fce38a3266179c
symmy596 <ars44@bath.ac.uk>
2019-02-19 21:24:10 -0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ author = 'Adam R.Symington'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
