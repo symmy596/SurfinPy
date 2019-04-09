8265143fe303bcd6e7493ed38060ac84d33e036f
symmy596 <ars44@bath.ac.uk>
2019-04-09 20:31:23 +0000

    Update test_p_vs_t.py

--- surfinpy/tests/test_p_vs_t.py
+++ surfinpy/tests/test_p_vs_t.py
@@ -49,7 +49,7 @@ class Testp_vs_t(unittest.TestCase):
 
     def test_initialise(self):
         x = ut.read_nist(test_data)
-        a, b, c, d = p_vs_t.inititalise(x, -10.0, 1000)
+        a, b, c, d = p_vs_t.inititalise(x, -10.0, 1000, -13, 5.5)
         assert_almost_equal(d[0], -10.000, decimal=3)
         assert_almost_equal(d[1], -10.000, decimal=3)
         assert_almost_equal(d[-1], -10.103, decimal=3)
