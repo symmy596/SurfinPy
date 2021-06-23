ZPE Calculation Fix
Fixed error causing ZPE not to be calculated properly.  Needs to be checked

surfinpy/data.py
@@ -38,7 +38,7 @@ class ReferenceDataSet():
         self.color = color
         self.entropy = entropy
         self.temp_range = temp_range
-        self.zpe = 0
+        self.zpev = 0
         self.svib = 0
         self.temperature = 0
         self.zpe = zpe
@@ -53,7 +53,7 @@ class ReferenceDataSet():
             self.temp_r = np.arange(self.temp_range[0],
                                     self.temp_range[1], 
                                     1, dtype="float")
-            self.zpe = vd.vib_calc(self.file, self.temp_r)[0]
+            self.zpev = vd.vib_calc(self.file, self.temp_r)[0]
             self.temperature = self.temp_r[0]
 
 class DataSet():
@@ -105,9 +105,10 @@ class DataSet():
         self.nspecies = nspecies
         self.entropy = entropy
         self.temp_range = temp_range
-        self.zpe = 0
+        self.zpev = 0
         self.svib = 0
         self.temperature = 0
+		self.zpe = zpe
         if self.entropy:
             self.temp_r = np.arange(self.temp_range[0],
                                     self.temp_range[1], 
@@ -119,5 +120,5 @@ class DataSet():
             self.temp_r = np.arange(self.temp_range[0],
                                     self.temp_range[1], 
                                     1, dtype="float")
-            self.zpe = vd.vib_calc(self.file, self.temp_r)[0]
+            self.zpev = vd.vib_calc(self.file, self.temp_r)[0]
             self.temperature = self.temp_r[0]